fd2bbfbaf0d453410d74c6256d5cb0c9fb4c8417
Robin Boutros <niuage@gmail.com>
2014-09-12 20:50:44 -0400

    having fun with a new design

---
+++ app/controllers/v2/home_controller.rb
@@ -0,0 +1,6 @@
+module V2
+  class HomeController < ::HomeController
+    layout "v2_application"
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'projects', to: 'home#projects', as: :projects
+
+  namespace :v2 do
+    root 'home#index'
+    get 'projects', to: 'home#projects', as: :projects
+  end
 end
